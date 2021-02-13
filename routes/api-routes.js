7f7ee6104193b6c99757a0f14b7abb49523a49ac
Pete-Scale <petescale@gmail.com>
2021-02-13 12:51:01 -0600

    GET and RENDER todos

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,7 +2,16 @@ const express = require('express')
 const router = express.Router()
 const db = require('../models')
 
-router.get('./api/todos', async (req, res) => {
+router.get('/api/todos', async (req, res) => {
+    try {
+        const todos = await db.Todo.findAll()
+        res.json(todos)
+    } catch(err) {
+        res.status(500).send(err)
+    }
+})
+
+router.post('/api/todos', async (req, res) => {
 
     try {
         await db.Todo.create(req.body)
@@ -11,8 +20,6 @@ router.get('./api/todos', async (req, res) => {
         res.status(500).send(err)
     }
 })
-
-// router.post()
 
 // router.put()
 
